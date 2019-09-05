d30ba9e1334256caef8d08bb83e03f7b840be574
Carolina Alarcon <ancaro1989@gmail.com>
2019-09-05 12:32:44 +0200

    Added missing MIT license text

--- src/Message/ConsumeInterface.php
+++ src/Message/ConsumeInterface.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Backq: Background tasks with workers & publishers via queues
+ *
+ * Copyright (c) 2013-2019 Sergei Shilko
+ *
+ * Distributed under the terms of the MIT License.
+ * Redistributions of files must retain the above copyright notice.
+ */
+
 namespace BackQ\Message;
 
 /**
