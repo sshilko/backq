8f97f8283a39c9f62de9df1ed0537a6a1619abdb
sshilko <sergey.shilko@gmail.com>
2019-09-23 12:09:37 +0200

    Add backq aws sns examples

---
+++ example/workers/amazon/sns/endpoints/remove.php
@@ -0,0 +1,19 @@
+<?php
+chdir(__DIR__);
+require_once('../endpoints.php');
+
+/**
+ * Remove AWS SNS endpoint
+ */
+
+$auth        = [];
+$platform    = strtoupper(basename($_SERVER['SCRIPT_FILENAME'], '.php'));
+$setupWorker = new endpoints($platform, $auth);
+$worker      = new BackQ\Worker\Amazon\SNS\Application\PlatformEndpoint\Remove(new \BackQ\Adapter\Beanstalk);
+
+$worker->setQueueName($worker->getQueueName() . $setupWorker->getPlatform());
+$worker->setClient($setupWorker->getClient());
+$worker->setRestartThreshold(500);
+$worker->setIdleTimeout(300);
+
+$worker->run();
